9345c869da880f724a35ef9091d303ea0a2d6d63
dtodt <danieltodt@gmail.com>
2020-01-19 23:38:57 -0300

    fix: color constant moved

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(CalculatorApp());
 
 class CalculatorApp extends StatelessWidget {
+  final Color primaryColor = Color(0xFF0A0E21);
+
   @override
   Widget build(BuildContext context) {
-    const primaryColor = Color(0xFF0A0E21);
-
     return MaterialApp(
       title: 'BMI CALCULATOR',
       theme: ThemeData.dark().copyWith(
