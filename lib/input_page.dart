df6b724c1e09c72db118eb46c581ce6fd39efbd5
dtodt <danieltodt@gmail.com>
2020-01-20 00:35:55 -0300

    feat: app layout

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,8 +1,7 @@
+import 'package:bmi_calculator/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class InputPage extends StatefulWidget {
-  InputPage({Key key}) : super(key: key);
-
   @override
   _InputPageState createState() => _InputPageState();
 }
@@ -14,9 +13,65 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: AppColors.secondary,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: AppColors.secondary,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              colour: AppColors.secondary,
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(
+                    colour: AppColors.secondary,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    colour: AppColors.secondary,
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({@required this.colour});
+
+  final Color colour;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      margin: EdgeInsets.all(15.0),
     );
   }
 }
