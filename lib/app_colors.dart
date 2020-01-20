7ebec37d9fb18b7b360acd7f92733964c2fc98fe
dtodt <danieltodt@gmail.com>
2020-01-19 23:58:36 -0300

    refactor: color constants extracted

---
+++ lib/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF0A0E21);
+  static const Color secondary = Color(0xFF1D1e33);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,17 @@
+import 'package:bmi_calculator/app_colors.dart';
 import 'package:bmi_calculator/input_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(CalculatorApp());
 
 class CalculatorApp extends StatelessWidget {
-  final Color primaryColor = Color(0xFF0A0E21);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BMI CALCULATOR',
       theme: ThemeData.dark().copyWith(
-        primaryColor: primaryColor,
-        scaffoldBackgroundColor: primaryColor,
+        primaryColor: AppColors.primary,
+        scaffoldBackgroundColor: AppColors.primary,
       ),
       home: InputPage(),
     );
